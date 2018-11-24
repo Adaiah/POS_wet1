dab2311bed19d41f60e9ff5e47f37aa8da38f52a
Adaiah <38374656+Adaiah@users.noreply.github.com>
2018-11-24 16:07:47 +0200

    WITH JOBS

--- commands.h
+++ commands.h
@@ -11,6 +11,15 @@
 #define MAX_LINE_SIZE 80
 #define MAX_ARG 20
 typedef enum { FALSE , TRUE } bool;
+typedef struct command {
+	string name;
+	int comm_id;
+	int PID;
+	int entry_time;
+	string status; //stopped or not
+	bool built_in; // Yes=true N=false
+}job_command, *pjob_command;
+
 int ExeComp(char* lineSize);
 int BgCmd(char* lineSize, void* jobs);
 int ExeCmd(void* jobs, char* lineSize, char* cmdString);
